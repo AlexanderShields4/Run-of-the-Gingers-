fcb78df6e3161ba6b974073ab57f21c6b3099905
alex <alshields1@davidson.edu>
2026-03-20 19:16:50 -0400

    updated charlies avatar

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -25,5 +25,5 @@ export const runners: Runner[] = [
     bio: "Last years Winner. Won't be the fastest off the line, but will never slow down.",
     speed_stat: 88,
     stamina_stat: 98,
-    image_url: "https://api.dicebear.com/9.x/avataaars/svg?seed=Liam&hairColor=d0021b,ff5c5c&skinColor=edb98a",
+    image_url: "https://api.dicebear.com/9.x/avataaars/svg?seed=Charlie&top=longHairNotTooLong&hairColor=c0392b&facialHair=moustacheMagnum&facialHairColor=c0392b&skinColor=edb98a",
   }];
